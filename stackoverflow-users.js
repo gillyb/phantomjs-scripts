d1c6d46d57ce360fd6e959c767a39ad6ac4f5ce0
gillyb <gillyb@gmail.com>
2013-01-31 21:35:47 +0200

    Adding script to scrape SO users

---
+++ stackoverflow-users.js
@@ -0,0 +1,86 @@
+var fs = require('fs');
+var system = require('system');
+    args = system.args;
+
+var page;
+var pageCount = 0;
+ 
+scanPage(pageCount);
+ 
+function scanPage(pageIndex) {
+  // dispose of page before moving on
+  if (typeof page !== 'undefined')
+    page.release();
+
+  // dispose of phantomjs if we're done
+  if (pageIndex > args[2]) {
+    phantom.exit();
+    return;
+  }
+
+  pageIndex++;
+
+  // start crawling...
+  page = require('webpage').create();
+  
+  var currentPage = 'http://stackoverflow.com/users?page=' + pageIndex + '&tab=reputation&filter=all';
+
+  page.open(currentPage, function(status) {
+    if (status === 'success') {
+      window.setTimeout(function() {
+        console.log('crawling page ' + pageIndex);
+
+        // scrape all users
+        var usersInfo = page.evaluate(function() {
+          var users = $('.user-info');
+
+          var userInfoList = [];
+          $.each(users, function(index, element) {
+            var userDetails = $(element).find('.user-details');
+            var userName = $(userDetails.find('a')[0]).html();
+            var userLink = $(userDetails.find('a')[0]).attr('href');
+            var userReputation = $(userDetails.find('.reputation-score')).html();
+            var userLocation = $(userDetails.find('.user-location')).html();
+
+            var userInfo = userReputation + '; ' + userName + '; ' + userLink + '; ' + userLocation;
+            userInfoList.push(userInfo);
+          });
+
+          return userInfoList;
+        });
+
+        // console.log(usersInfo);
+
+        usersInfo.forEach(function(element, i) {
+          if (element.toLowerCase().indexOf(args[1].toLowerCase()) !== -1) {
+            console.log('saving to file : ' + element);
+            fs.write('users.txt', element + '\n', 'a');
+          }
+        })
+
+        scanPage(pageIndex);
+      }, 3000);
+    }
+    else {
+      console.log('error crawling page ' + pageIndex);
+      page.release();
+    }
+  });
+}
+
+// This will go over the whole list, filter the ones we asked for
+// and write them to a file
+function filterAndSave(users) {
+  users.forEach(function(element, index) {
+    
+
+    var userInfo = userReputation + '; ' + userName + '; ' + userLink + '; ' + userLocation;
+    console.log(userInfo);
+
+    if (userLocation.indexOf(args[0]) > -1) {
+      //var userInfo = userReputation + '; ' + userName + '; ' + userLink + '; ' + userLocation;
+      console.log(userInfo);
+      fs.write('users.txt', userInfo, 'a');
+    }
+  });
+}
